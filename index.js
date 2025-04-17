022f2cb04fb114e3f6db15f4c8125a001086a568
Bilal <cloud4invertio@gmail.com>
2025-04-17 11:55:17 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ port = 3000;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
